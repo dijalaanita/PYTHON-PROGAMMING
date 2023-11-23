4a0ec2d90c3163f4edd3f4da881e49767de3e07d
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2023-11-23 13:16:50 +0000

    added comments

--- ex3.py
+++ ex3.py
@@ -1,23 +1,23 @@
-print("I will now count my chickens:")
+print("I will now count my chickens:")#prints text
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25 + 30 / 6)#prints text and calculates equation
+print("Roosters", 100 - 25 * 3 % 4)#prints text and calculation answer
 
-print("Now I will count the eggs:")
+print("Now I will count the eggs:")#prints text
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)#prints answer of calculation
 
-print("Is it true that 3 + 2 < 5 - 7?")
+print("Is it true that 3 + 2 < 5 - 7?")#prints text
 
 print(3 + 2 < 5 - 7)#this prints out a truth value. '<' is a boolean operator
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print("What is 3 + 2?", 3 + 2)#prints text and answer for calculation
+print("What is 5 - 7?", 5 - 7)#prints text and answer for calculation
 
-print("Oh, that's why it's False.")
+print("Oh, that's why it's False.")#prints text
 
-print("How about some more.")
+print("How about some more.")#prints text
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less than or equal?", 5 <= -2)
+print("Is it greater?", 5 > -2)#prints text and truth value
+print("Is it greater or equal?", 5 >= -2)#prints text and truth value
+print("Is it less than or equal?", 5 <= -2)#prints text and truth value
