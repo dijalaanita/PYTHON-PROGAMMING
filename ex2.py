a8e293d84de59dca1e8070cccf8195b1a2621ed7
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2023-11-23 13:00:56 +0000

    added '.'

--- ex2.py
+++ ex2.py
@@ -5,4 +5,4 @@ print("I could have code like this") # and the comment after is ignored
 
 #you can also use a comment to 'disable' or comment out code:
 # print ("This will run")
-print("This will run")
+print("This will run.")
