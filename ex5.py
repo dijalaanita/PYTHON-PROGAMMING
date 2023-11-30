67f7f8547cf03f9390c9e80d1c0324c1a0369c81
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2023-11-30 15:10:40 +0000

    new file

---
+++ ex5.py
@@ -0,0 +1,23 @@
+myName = 'Anita Dijala'
+myAge = 35 #its a lie
+myHeight = 165
+myWeight = 70
+myEyes = 'Brown'
+myTeeth = 'White'
+myHair = 'Black and Blonde'
+
+print(f"My name is {myName}")
+print(f"I am {myAge} years old")
+print(f"I am {myHeight} cm tall")
+print("It is quite short but idc")
+print(f"I have {myEyes} coloured eyes")
+print(f"my hair is {myHair}")
+
+total = myAge + myWeight + myHeight
+print(f"If i add my height, weight, and age, i'll get, {total}")
+
+inches = myHeight / 2.54
+print(f"My height in inches is {round(inches)}")
+
+pound = myWeight * 2.205
+print(f"My weight in pounds is {pound}")
