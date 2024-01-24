ef2714432e38d2ad1bdc005bbabd81d6897d5f6f
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2024-01-24 12:38:39 +0000

    new

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv #argv = argument variable
+
+print("this is called ", script)
+print("first vari is ", first)
+print("secind vari is ", second)
+print("third vari is ", third)
