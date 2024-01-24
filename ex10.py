cade607e0379df98cf5735c9503e47444bf8ba39
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2024-01-24 12:38:03 +0000

    new

---
+++ ex10.py
@@ -0,0 +1,21 @@
+tabby_cat = "\tI'm tabbed in."
+persian = "i'm split\non a line."
+backlash = "i'm \\ a \\ cat."
+
+fat = '''
+i'll do a list:
+\t * cat food
+\t * fish
+\t * catnip \n\t * grass
+'''
+
+print(tabby_cat)
+print(persian)
+print(backlash)
+print(fat)
+
+# \\ -> backlash
+# \' -> single quote
+# \" -> double quote
+# \t -> horizontal tab, bullet points basically
+# \v -> vertical tab
