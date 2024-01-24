053a626cffb97225b72c6310d5b3bf43b63d9ca8
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2024-01-24 12:38:53 +0000

    new files

---
+++ selfTask1.py
@@ -0,0 +1,6 @@
+bean = "{} {} {} {}"
+ban = 4
+
+print(bean.format("one\'s", "two\'s", "three\'s", "four\'s"))
+print(bean.format("* cat food\n", "* fish\n", "* catnip\n", "* grass\n"))
+print(bean.format(ban, ban, ban, ban))
---
+++ selftask2.py
@@ -0,0 +1,10 @@
+print("enter number 1", end = ' ')
+num1 = int(input())
+print("enter number 2", end = ' ')
+num2 = int(input())
+
+sum = num1 + num2
+sub = num2 - num1
+mul = num1 * num2
+sqr = num2 * num2
+print(sum, sub, mul, sqr)
