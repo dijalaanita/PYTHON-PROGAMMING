7f27a277e03e4dd905c1e10fcfbe415908217837
dijalaanita <100611369+dijalaanita@users.noreply.github.com>
2024-01-24 12:38:27 +0000

    new

---
+++ ex12.py
@@ -0,0 +1,10 @@
+#print("how old are you: ")#,end = ' ')
+age = input("age? ")
+#print("what's your height: ", end = ' ')
+height = input("height? ")
+#print("how much do you weigh?", end = ' ')
+weight = input("weight? ")
+
+print(f" so you are {age} years old and you are {height} tall and {weight} heavy")
+
+#end = ' ' means to not end the line with a newline
